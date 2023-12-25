34391488be891e11d6a8f5513ca29559c1c6adf4
Emily Krohn <emilykrohn2@gmail.com>
2023-12-25 08:05:23 -0800

    Shows tree before drops and fixed bugs
    Lock rotation and freeze tree to stop gravity while the player hasn't dropped the tree yet. Changed the layer the player is on and the trees that haven't dropped yet to not affect trees that are dropping

--- Scripts/drop.gd
+++ Scripts/drop.gd
@@ -4,13 +4,27 @@ var trees = [preload("res://Scenes/Game/Trees/tree_1.tscn"), preload("res://Scen
 			 preload("res://Scenes/Game/Trees/tree_3.tscn"), preload("res://Scenes/Game/Trees/tree_4.tscn")]
 
 var spawn_cooldown := true
+var has_dropped := true
+var tree
 
 func spawn_tree():
-	if spawn_cooldown and Input.is_action_just_pressed("ui_select"):
-		var tree = trees[randi() % trees.size()].instantiate()
+	if spawn_cooldown and has_dropped:
+		tree = trees[randi() % trees.size()].instantiate()
+		tree.freeze = true
+		tree.lock_rotation = true
 		tree.global_position = $"../drop_point".global_position
+		tree.collision_layer = 2
 		$"../../../../".add_child(tree)
 		spawn_cooldown = false
+		has_dropped = false
+	else:
+		tree.global_position = $"../drop_point".global_position
+
+func drop_tree():
+	tree.collision_layer = 1
+	tree.freeze = false
+	tree.lock_rotation = false
+	has_dropped = true
 
 func _on_timer_timeout():
 	spawn_cooldown = true
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -5,3 +5,5 @@ const SPEED := 10
 func _physics_process(_delta):
 	$movement.move(SPEED)
 	$drop.spawn_tree()
+	if Input.is_action_just_pressed("ui_select"):
+		$drop.drop_tree()
