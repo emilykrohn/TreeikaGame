7e34c928da18ca1a769c942e146909fcd3f7f354
Emily Krohn <emilykrohn2@gmail.com>
2023-12-25 08:11:21 -0800

    Stop player movement while tree dropping

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,9 +1,15 @@
 extends CharacterBody2D
 
 const SPEED := 10
+var can_move = true
 
 func _physics_process(_delta):
-	$movement.move(SPEED)
 	$drop.spawn_tree()
 	if Input.is_action_just_pressed("ui_select"):
+		can_move = false
 		$drop.drop_tree()
+	if can_move:
+		$movement.move(SPEED)
+
+func _on_move_cooldown_timeout():
+	can_move = true
