7bfb73ad6fe96a5c554e6d314630bcde958596d4
Emily Krohn <emilykrohn2@gmail.com>
2023-12-27 10:21:19 -0800

    Combine but doesn't delete other tree

--- Scripts/Trees/tree_1.gd
+++ Scripts/Trees/tree_1.gd
@@ -1,9 +1,15 @@
 extends RigidBody2D
 
+var can_combine := false
+
 func _on_area_2d_body_entered(body):
-	if body.name == "Tree1":
+	if body.name == "Tree1" and can_combine:
 		print("tree1entered")
 		var tree2 = preload("res://Scenes/Game/Trees/tree_2.tscn").instantiate()
 		tree2.call_deferred("set_position", position)
 		get_parent().call_deferred("add_child", tree2)
 		queue_free()
+
+func _on_area_2d_area_entered(area):
+	if area.name == "combine_area":
+		can_combine = true
