1db668d9c4d90c39128af666e6ced14600cfacd6
Emily Krohn <emilykrohn2@gmail.com>
2023-12-30 13:31:25 -0800

    Add background Music

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -3,6 +3,9 @@ extends Node
 var collision = false
 var tree
 
+func _ready():
+	$Music.play()
+
 func _process(_delta):
 	if collision:
 		combine_trees()
@@ -20,3 +23,6 @@ func _on_button_pressed():
 	$ButtonSound.play()
 	$title_screen.visible = false
 	$game.visible = true
+
+func _on_music_finished():
+	$Music.play()
