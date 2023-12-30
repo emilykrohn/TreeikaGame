0368e046837bf5d69eaf3fea79cad7f6646feb27
Emily Krohn <emilykrohn2@gmail.com>
2023-12-30 12:30:03 -0800

    Image shows next tree instead of current

--- Scripts/drop.gd
+++ Scripts/drop.gd
@@ -6,43 +6,52 @@ var trees = [preload("res://Scenes/Game/Trees/tree_1.tscn"), preload("res://Scen
 var has_dropped := true
 var can_change_tree := false
 var is_tree := false
-var tree
+var current_tree = trees[randi() % trees.size()].instantiate()
+var next_tree
 
 func spawn_tree():
 	if has_dropped:
-		tree = trees[randi() % trees.size()].instantiate()
-		if tree.name == "Tree1":
-			tree.add_to_group("tree1")
+		next_tree = trees[randi() % trees.size()].instantiate()
+
+		if current_tree.name == "Tree1":
+			current_tree.add_to_group("tree1")
+		elif current_tree.name == "Tree2":
+			current_tree.add_to_group("tree2")
+		elif current_tree.name == "Tree3":
+			current_tree.add_to_group("tree3")
+		elif current_tree.name == "Tree4":
+			current_tree.add_to_group("tree4")
+
+		if next_tree.name == "Tree1":
 			$"../../../../ui/Panel2/VBoxContainer/BoxContainer/TextureRect".texture = load("res://Art/Tree1.png")
-		elif tree.name == "Tree2":
-			tree.add_to_group("tree2")
+		elif next_tree.name == "Tree2":
 			$"../../../../ui/Panel2/VBoxContainer/BoxContainer/TextureRect".texture = load("res://Art/Tree2.png")
-		elif tree.name == "Tree3":
-			tree.add_to_group("tree3")
+		elif next_tree.name == "Tree3":
 			$"../../../../ui/Panel2/VBoxContainer/BoxContainer/TextureRect".texture = load("res://Art/Tree3.png")
-		elif tree.name == "Tree4":
-			tree.add_to_group("tree4")
+		elif next_tree.name == "Tree4":
 			$"../../../../ui/Panel2/VBoxContainer/BoxContainer/TextureRect".texture = load("res://Art/Tree4.png")
-		tree.freeze = true
-		tree.lock_rotation = true
-		tree.global_position = $"../drop_point".global_position
-		tree.collision_layer = 2
-		$"../../../../".add_child(tree)
+			
+		current_tree.freeze = true
+		current_tree.lock_rotation = true
+		current_tree.global_position = $"../drop_point".global_position
+		current_tree.collision_layer = 2
+		$"../../../../".add_child(current_tree)
 		has_dropped = false
 		is_tree = true
 
 func move_tree():
 	if is_tree:
-		tree.global_position = $"../drop_point".global_position
+		current_tree.global_position = $"../drop_point".global_position
 
 func drop_tree():
 	if is_tree:
-		tree.collision_layer = 1
-		tree.freeze = false
-		tree.lock_rotation = false
+		current_tree.collision_layer = 1
+		current_tree.freeze = false
+		current_tree.lock_rotation = false
 		can_change_tree = false
 		has_dropped = true
-		is_tree = false 
+		is_tree = false
+		current_tree = next_tree
 
 func _on_timer_2_timeout():
 	can_change_tree = true
